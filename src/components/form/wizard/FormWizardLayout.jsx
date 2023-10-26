6f769bc72900ba9cd88e7af443c17484d0b19e1d
chris <5sensprod@gmail.com>
2023-10-26 11:53:16 +0200

    ajout label step

---
+++ src/components/form/wizard/FormWizardLayout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Card, Container } from 'react-bootstrap'
+import { useFormWizardState } from './FormWizardContext'
+import StepProgressIndicator from './StepProgressIndicator'
+import WizardNavigationButtons from './WizardNavigationButtons'
+
+const FormWizardLayout = ({ children }) => {
+  const { state } = useFormWizardState()
+
+  return (
+    <Card className="p-4 shadow-lg bg-dark text-light">
+      <Card.Body>
+        <Container>
+          <StepProgressIndicator />
+          <h3 className="mb-3">
+            {state.steps[state.currentStep - 1].label}
+          </h3>{' '}
+          {/* Afficher le label de l'étape active */}
+          {children}{' '}
+          {/* C'est ici que votre composant d'étape actif sera rendu */}
+          <WizardNavigationButtons />
+        </Container>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default FormWizardLayout
--- src/components/form/wizard/index.js
+++ src/components/form/wizard/index.js
@@ -1,33 +1,24 @@
 import React from 'react'
-import { Card, Container } from 'react-bootstrap'
+import FormWizardLayout from './FormWizardLayout'
 import { FormWizardProvider, useFormWizardState } from './FormWizardContext'
-import StepProgressIndicator from './StepProgressIndicator'
-import WizardNavigationButtons from './WizardNavigationButtons'
 
 const FormWizard = ({ steps }) => {
   return (
     <FormWizardProvider steps={steps}>
-      <FormWizardLayout />
+      <WizardContent />
     </FormWizardProvider>
   )
 }
 
-const FormWizardLayout = () => {
+const WizardContent = () => {
   const { state } = useFormWizardState()
-
   const ActiveStepComponent = state.steps[state.currentStep - 1].component
   const activeStepProps = state.steps[state.currentStep - 1].props
 
   return (
-    <Card className="p-4 shadow-lg bg-dark text-light">
-      <Card.Body>
-        <Container>
-          <StepProgressIndicator />
-          {React.createElement(ActiveStepComponent, activeStepProps)}
-          <WizardNavigationButtons />
-        </Container>
-      </Card.Body>
-    </Card>
+    <FormWizardLayout>
+      <ActiveStepComponent {...activeStepProps} />
+    </FormWizardLayout>
   )
 }
 
