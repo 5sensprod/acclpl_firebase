363570d54b6b018ad8881cbb3103072038ff99f0
chris <5sensprod@gmail.com>
2023-10-31 21:23:12 +0100

    refacto

--- src/services/establishmentCheckerService.js
+++ src/services/establishmentCheckerService.js
@@ -25,84 +25,84 @@ async function checkDuplicateEstablishment(
 
   const querySnapshot = await getDocs(establishmentQuery)
 
-  // Aucun établissement trouvé
-  if (querySnapshot.empty) {
-    dispatch({ type: 'SET_ESTABLISHMENT_EXISTS', payload: false })
-    dispatch({ type: 'SET_CURRENT_ESTABLISHMENT_ID', payload: null })
-    return false
-  }
+  switch (querySnapshot.docs.length) {
+    case 0: // Aucun établissement trouvé
+      dispatch({ type: 'SET_ESTABLISHMENT_EXISTS', payload: false })
+      dispatch({ type: 'SET_CURRENT_ESTABLISHMENT_ID', payload: null })
+      return false
 
-  // Plusieurs établissements trouvés
-  if (querySnapshot.docs.length > 1) {
-    const establishmentIds = querySnapshot.docs.map((doc) => doc.id)
-    dispatch({ type: 'SET_ESTABLISHMENT_EXISTS', payload: true })
-    dispatch({
-      type: 'SET_CURRENT_ESTABLISHMENT_IDS',
-      payload: establishmentIds,
-    })
-    console.log('Establishment IDs:', establishmentIds)
-    return { found: true, multiple: true, establishmentIds }
-  }
+    case 1: // Un seul établissement trouvé
+      const establishmentDoc = querySnapshot.docs[0]
+      const establishmentId = establishmentDoc.id
+      console.log('Establishment ID:', establishmentId)
+      const establishmentData = establishmentDoc.data()
+      dispatch({ type: 'SET_ESTABLISHMENT_EXISTS', payload: true })
+      dispatch({
+        type: 'SET_CURRENT_ESTABLISHMENT_ID',
+        payload: establishmentId,
+      })
 
-  // Un seul établissement trouvé
-  const establishmentDoc = querySnapshot.docs[0]
-  const establishmentId = establishmentDoc.id
-  const establishmentData = establishmentDoc.data()
+      const observationsQuery = query(
+        collection(
+          firestore,
+          'establishments',
+          establishmentId,
+          'observations',
+        ),
+        orderBy('date', 'desc'),
+        limit(1),
+      )
+      const observationsSnapshot = await getDocs(observationsQuery)
+      const photoURL =
+        observationsSnapshot.empty ||
+        !observationsSnapshot.docs[0].data().photoURLs?.length
+          ? null
+          : observationsSnapshot.docs[0].data().photoURLs[0]
 
-  dispatch({ type: 'SET_ESTABLISHMENT_EXISTS', payload: true })
-  dispatch({ type: 'SET_CURRENT_ESTABLISHMENT_ID', payload: establishmentId })
+      const streetId =
+        typeof establishmentData.streetRef === 'string'
+          ? establishmentData.streetRef
+          : establishmentData.streetRef.id
 
-  console.log('Establishment ID:', establishmentId)
+      if (!streetId) {
+        throw new Error('Unable to extract street ID from the reference.')
+      }
 
-  const observationsQuery = query(
-    collection(firestore, 'establishments', establishmentId, 'observations'),
-    orderBy('date', 'desc'),
-    limit(1),
-  )
+      const streetDocRef = doc(firestore, 'streets', streetId)
+      const streetDoc = await getDoc(streetDocRef)
 
-  const observationsSnapshot = await getDocs(observationsQuery)
+      if (!streetDoc.exists) {
+        throw new Error('Associated street document not found.')
+      }
 
-  let photoURL = null
-  if (!observationsSnapshot.empty) {
-    const observationData = observationsSnapshot.docs[0].data()
-    if (observationData.photoURLs && observationData.photoURLs.length > 0) {
-      photoURL = observationData.photoURLs[0]
-    }
-  }
+      const streetData = streetDoc.data()
 
-  const streetId =
-    typeof establishmentData.streetRef === 'string'
-      ? establishmentData.streetRef
-      : establishmentData.streetRef.id
+      return {
+        found: true,
+        details: {
+          establishmentId,
+          establishmentName: establishmentData.establishmentName,
+          streetName: streetData.streetName,
+          city: streetData.city,
+          postalCode: streetData.postalCode,
+          streetNumber: establishmentData.streetNumber,
+          photoURL,
+          coordinates: [
+            establishmentData.coordinates.longitude,
+            establishmentData.coordinates.latitude,
+          ],
+        },
+      }
 
-  if (!streetId) {
-    throw new Error('Unable to extract street ID from the reference.')
-  }
-
-  const streetDocRef = doc(firestore, 'streets', streetId)
-  const streetDoc = await getDoc(streetDocRef)
-
-  if (!streetDoc.exists) {
-    throw new Error('Associated street document not found.')
-  }
-
-  const streetData = streetDoc.data()
-
-  return {
-    found: true,
-    details: {
-      establishmentId: establishmentId,
-      establishmentName: establishmentData.establishmentName,
-      streetName: streetData.streetName,
-      city: streetData.city,
-      postalCode: streetData.postalCode,
-      streetNumber: establishmentData.streetNumber,
-      photoURL: photoURL,
-      coordinates: [
-        establishmentData.coordinates.longitude,
-        establishmentData.coordinates.latitude,
-      ],
-    },
+    default: // Plusieurs établissements trouvés
+      const establishmentIds = querySnapshot.docs.map((doc) => doc.id)
+      console.log('Establishment IDs:', establishmentIds)
+      dispatch({ type: 'SET_ESTABLISHMENT_EXISTS', payload: true })
+      dispatch({
+        type: 'SET_CURRENT_ESTABLISHMENT_IDS',
+        payload: establishmentIds,
+      })
+      return { found: true, multiple: true, establishmentIds }
   }
 }
 
