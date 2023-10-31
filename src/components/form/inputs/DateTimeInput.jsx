4f91059349cc19347aa72916ed0f897a2854e0aa
chris <5sensprod@gmail.com>
2023-10-31 13:15:45 +0100

    refacto

--- src/components/form/inputs/DateTimeInput.jsx
+++ src/components/form/inputs/DateTimeInput.jsx
@@ -6,17 +6,10 @@ import { useFormWizardState } from '../wizard/FormWizardContext'
 const DateTimeInput = () => {
   const { dispatch, state } = useFormWizardState()
 
-  const handleDateChange = (event) => {
+  const handleInputChange = (field) => (event) => {
     dispatch({
       type: 'UPDATE_FORM_DATA',
-      payload: { dateOfObservation: event.target.value },
-    })
-  }
-
-  const handleTimeChange = (event) => {
-    dispatch({
-      type: 'UPDATE_FORM_DATA',
-      payload: { timeOfObservation: event.target.value },
+      payload: { [field]: event.target.value },
     })
   }
 
@@ -25,45 +18,62 @@ const DateTimeInput = () => {
     const formattedDate = currentDate.toISOString().slice(0, 10)
     const formattedTime = currentDate.toTimeString().slice(0, 5)
 
-    handleDateChange({ target: { value: formattedDate } })
-    handleTimeChange({ target: { value: formattedTime } })
+    handleInputChange('dateOfObservation')({ target: { value: formattedDate } })
+    handleInputChange('timeOfObservation')({ target: { value: formattedTime } })
   }
 
   return (
     <div className="d-flex justify-content-between mb-0 gap-3">
-      <div className="mb-0 w-50">
-        <InputWrapper label="Date" id="date-input">
-          <input
-            id="date-input"
-            type="date"
-            className="form-control"
-            value={state.formData.dateOfObservation}
-            onChange={handleDateChange}
-          />
-          <Button
-            variant="link"
-            className="btn text-light mb-0 p-1 ps-0 pb-0 "
-            style={{ textDecoration: 'none' }}
-            onClick={setToCurrentDateTime}
-          >
-            Maintenant
-          </Button>
-        </InputWrapper>
-      </div>
-
-      <div className="mb-0 w-50">
-        <InputWrapper label="Heure" id="time-input">
-          <input
-            id="time-input"
-            type="time"
-            className="form-control"
-            value={state.formData.timeOfObservation}
-            onChange={handleTimeChange}
-          />
-        </InputWrapper>
-      </div>
+      <DateTimeSection
+        label="Date"
+        id="date-input"
+        type="date"
+        value={state.formData.dateOfObservation}
+        onChange={handleInputChange('dateOfObservation')}
+        buttonLabel="Maintenant"
+        onClickButton={setToCurrentDateTime}
+      />
+      <DateTimeSection
+        label="Heure"
+        id="time-input"
+        type="time"
+        value={state.formData.timeOfObservation}
+        onChange={handleInputChange('timeOfObservation')}
+      />
     </div>
   )
 }
+
+const DateTimeSection = ({
+  label,
+  id,
+  type,
+  value,
+  onChange,
+  buttonLabel,
+  onClickButton,
+}) => (
+  <div className="mb-0 w-50">
+    <InputWrapper label={label} id={id}>
+      <input
+        id={id}
+        type={type}
+        className="form-control"
+        value={value}
+        onChange={onChange}
+      />
+      {buttonLabel && (
+        <Button
+          variant="link"
+          className="btn text-light mb-0 p-1 ps-0 pb-0"
+          style={{ textDecoration: 'none' }}
+          onClick={onClickButton}
+        >
+          {buttonLabel}
+        </Button>
+      )}
+    </InputWrapper>
+  </div>
+)
 
 export default DateTimeInput
