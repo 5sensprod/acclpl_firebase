ebf61e116926116addc32cfe8cf7302cd4346581
chris <5sensprod@gmail.com>
2023-10-31 14:20:17 +0100

    nettoyage

--- src/components/form/inputs/GeolocateAddress.jsx
+++ src/components/form/inputs/GeolocateAddress.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import { InputWrapper, StyledInput } from './InputWrapper'
-import useGeolocationAddress from './useGeolocationAddress'
+import useGeolocationAddress from '../hooks/useGeolocationAddress'
 import { useFormWizardState } from '../context/FormWizardContext'
 
 const GeolocateAddress = (props) => {
